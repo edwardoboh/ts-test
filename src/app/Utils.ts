06f7e1494828eac2ae01ca9aae5bb4ecf987eaa6
edwardoboh <obohedward@gmail.com>
2023-09-12 12:50:32 +0100

    feat: make use of  other jest matchers

--- src/app/Utils.ts
+++ src/app/Utils.ts
@@ -2,3 +2,21 @@
 export function convertToUpper(input: string): string{
     return input.toUpperCase()
 }
+
+export function getStringInfo(arg: string): StringInfo{
+    return {
+        lower: arg.toLowerCase(),
+        upper: arg.toUpperCase(),
+        characters: Array.from(arg),
+        length: arg.length,
+        metedata: {}
+    }
+}
+
+export type StringInfo = {
+    lower: string,
+    upper: string,
+    characters: string[],
+    length: number,
+    metedata: Object | undefined
+}
--- src/test/Utils.test.ts
+++ src/test/Utils.test.ts
@@ -1,4 +1,4 @@
-import { convertToUpper } from "../app/Utils"
+import { convertToUpper, getStringInfo } from "../app/Utils"
 
 describe('Utils test Suite', () => {
     it('Should convert string to Uppercase', () => {
@@ -8,5 +8,20 @@ describe('Utils test Suite', () => {
         const actual = sut('abc')
 
         expect(actual).toBe(expected)
+    })
+
+    it.only('Return information about a string', () => {
+        const sut = getStringInfo;
+        
+        const actual = sut('Test-Me')
+
+        expect(actual.length).toBe(7)
+        expect(actual.metedata).toEqual({})
+
+        expect(actual.characters).toHaveLength(7)
+        expect(actual.characters).toContain<string>('M')
+
+        expect(actual.characters).toEqual(['T', 'e', 's', 't', '-', 'M', 'e'])
+        expect(actual.characters).toEqual(expect.arrayContaining(['-', 'M', 'e', 'T', 'e', 's', 't',]))
     })
 })
