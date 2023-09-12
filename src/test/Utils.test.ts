ad43627804753e7c40394c8d86685d07aae817bc
edwardoboh <obohedward@gmail.com>
2023-09-12 13:15:43 +0100

    feat: structure the tests better

--- src/test/Utils.test.ts
+++ src/test/Utils.test.ts
@@ -10,18 +10,32 @@ describe('Utils test Suite', () => {
         expect(actual).toBe(expected)
     })
 
-    it.only('Return information about a string', () => {
-        const sut = getStringInfo;
-        
-        const actual = sut('Test-Me')
+    describe('getStringInfo for arg Test-me should', () => {
+        test('Should have the right length', () => {
+            const actual = getStringInfo('Test-Me')
+            expect(actual.length).toBe(7)
+        });
+        test('metadata should equal {}', () => {
+            const actual = getStringInfo('Test-Me')
+            expect(actual.metedata).toEqual({})
+        });
+        test('character property array should contain M', () => {
+            const actual = getStringInfo('Test-Me')
+            expect(actual.characters).toContain<string>('M')
+        });
+        test('character property array to equal', () => {
+            const actual = getStringInfo('Test-Me')
+            expect(actual.characters).toEqual(['T', 'e', 's', 't', '-', 'M', 'e'])
+            expect(actual.characters).toEqual(expect.arrayContaining(['-', 'M', 'e', 'T', 'e', 's', 't',]))
+        });
+        test('metadata property to be defined', () => {
+            const actual = getStringInfo('Test-Me')
 
-        expect(actual.length).toBe(7)
-        expect(actual.metedata).toEqual({})
+            expect(actual.metedata).not.toBe(undefined)
+            expect(actual.metedata).not.toBeUndefined()
+            expect(actual.metedata).toBeDefined()
+            expect(actual.metedata).toBeTruthy()
+        })
 
-        expect(actual.characters).toHaveLength(7)
-        expect(actual.characters).toContain<string>('M')
-
-        expect(actual.characters).toEqual(['T', 'e', 's', 't', '-', 'M', 'e'])
-        expect(actual.characters).toEqual(expect.arrayContaining(['-', 'M', 'e', 'T', 'e', 's', 't',]))
     })
 })
