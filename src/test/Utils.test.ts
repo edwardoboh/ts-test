a941b2635ed67e1d97087a306c0dabf38d7605e3
edwardoboh <obohedward@gmail.com>
2023-09-20 23:14:55 +0100

    chore: complete first testing module

--- src/test/Utils.test.ts
+++ src/test/Utils.test.ts
@@ -8,7 +8,7 @@ describe('Utils test Suite', () => {
         const actual = sut('abc')
 
         expect(actual).toBe(expected)
-    })
+    });
 
     describe('getStringInfo for arg Test-me should', () => {
         test('Should have the right length', () => {
@@ -37,5 +37,20 @@ describe('Utils test Suite', () => {
             expect(actual.metedata).toBeTruthy()
         })
 
-    })
+    });
+
+    describe.only('parameterized test for convertToUpper', () => {
+        it.each([
+            {input: 'my-name', expected: 'MY-NAME'},
+            {input: 'My-Name', expected: 'MY-NAME'},
+            {input: 'my-nAmE', expected: 'MY-NAME'}
+        ])('Should convert $input to $expected', ({input, expected}) => {
+            const sut = convertToUpper;
+    
+            const actual = sut(input)
+    
+            expect(actual).toBe(expected)
+        });
+    });
+
 })
